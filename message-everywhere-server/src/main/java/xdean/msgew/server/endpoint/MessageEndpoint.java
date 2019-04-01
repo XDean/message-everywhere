21f09853efa78022cac9cc2762fd1fd34e03ac95
dxu <dean.xu@asml.com>
2019-04-01 14:29:44 +0800

    observe message

--- message-everywhere-server/src/main/java/xdean/msgew/server/endpoint/MessageEndpoint.java
+++ message-everywhere-server/src/main/java/xdean/msgew/server/endpoint/MessageEndpoint.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.reactivex.Observable;
 import xdean.msgew.server.model.Message;
 import xdean.msgew.server.service.MessageService;
 
@@ -25,5 +28,11 @@ public class MessageEndpoint {
   @PostMapping("/fetch")
   public List<Message> fetch(@RequestParam("limit") int limit, @RequestParam("offset") int offset) {
     return service.fetch().skip(offset).limit(limit).toList().blockingGet();
+  }
+
+  @GetMapping
+  public Observable<ServerSentEvent<Message>> observe() {
+    return service.observe()
+        .map(msg -> ServerSentEvent.builder(msg).build());
   }
 }
