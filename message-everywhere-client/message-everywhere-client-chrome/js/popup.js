9196cf49bdecdefa7f869ff7f9ef1c11981bca8f
dxu <dean.xu@asml.com>
2019-04-02 16:37:44 +0800

    auto fetch msg and add li

--- message-everywhere-client/message-everywhere-client-chrome/js/popup.js
+++ message-everywhere-client/message-everywhere-client-chrome/js/popup.js
@@ -29,7 +29,23 @@ var observeMsg = event => {
             console.warn('Msg observer error', event)
         }
         observe.onmessage = event => {
-            console.debug('Recieve msg', event.data)
+            var msg = JSON.parse(event.data)
+            console.debug('Recieve msg', msg)
+            var messageList = document.querySelector('.message-list')
+
+            var newItem = document.createElement('li')
+            var sender = document.createElement('span')
+            sender.className = 'msg-sender'
+            sender.appendChild(document.createTextNode(msg.from.name))
+            newItem.appendChild(sender)
+            var content = document.createElement('span')
+            content.className = 'msg-content'
+            content.appendChild(document.createTextNode(msg.content))
+            newItem.appendChild(content)
+            var dateTime = new Date();
+            dateTime.setTime(msg.timestamp * 1000);
+            newItem.title = dateTime.toLocaleDateString()
+            messageList.appendChild(newItem)
         }
     } else if (observe) {
         console.debug('To close observe')
