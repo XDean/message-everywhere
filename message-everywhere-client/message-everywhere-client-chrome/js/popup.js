708f882972b14caf19f982713eb6b7cb5f7059ed
dxu <dean.xu@asml.com>
2019-04-02 15:56:40 +0800

    auto fetch msg by sse

--- message-everywhere-client/message-everywhere-client-chrome/js/popup.js
+++ message-everywhere-client/message-everywhere-client-chrome/js/popup.js
@@ -1,7 +1,38 @@
 const server = 'http://127.0.0.1:8080/message-everywhere/'
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('query').onclick = query
+    document.getElementById('fetch').onclick = fetchMsg
+    document.getElementById('autoFetch').onclick = observeMsg
 })
 
-var query = element => fetch(server + 'hello').then(d => console.log(d))
+var fetchMsg = event => fetch(server + 'hello').then(d => {
+    var messageList = document.querySelector('.message-list')
+    var newItem = document.createElement('li')
+    newItem.appendChild(document.createTextNode('hello'))
+    messageList.appendChild(newItem)
+})
+
+var observe = null
+var observeMsg = event => {
+    var checked = event.srcElement.checked
+    console.debug('Auto Fetch: ', event, checked)
+    if (checked) {
+        if (observe && observe.readyState != EventSource.CLOSED) {
+            console.debug('Observer exists, return directly')
+            return
+        }
+        observe = new EventSource(server + 'observe')
+        observe.onopen = event => {
+            console.info('Start observe msg', event)
+        }
+        observe.onerror = event => {
+            console.warn('Msg observer error', event)
+        }
+        observe.onmessage = event => {
+            console.debug('Recieve msg', event.data)
+        }
+    } else if (observe) {
+        console.debug('To close observe')
+        observe.close()
+    }
+}
