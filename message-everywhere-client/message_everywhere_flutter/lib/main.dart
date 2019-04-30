e83207f1db58164d2c543312ab86d144e976444c
Dean Xu <373216024@qq.com>
2019-04-30 14:31:14 +0800

    remove android_alaram_manager use service

--- message-everywhere-client/message_everywhere_flutter/lib/main.dart
+++ message-everywhere-client/message_everywhere_flutter/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'dart:isolate';
 
-void main() => runApp(new MyApp());
+void main() async {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -17,7 +22,12 @@ class Settings extends StatefulWidget {
 }
 
 class SettingsState extends State<Settings> {
-  bool _value = false;
+  static const platform = const MethodChannel("message-everywhere-fetch");
+
+  String _url = "";
+  String _encode = "";
+  bool _run = false;
+  SendPort _port;
 
   @override
   Widget build(BuildContext context) {
@@ -33,11 +43,13 @@ class SettingsState extends State<Settings> {
                 children: [
                   TextFormField(
                       keyboardType: TextInputType.url,
+                      onSaved: (s) => _url = s,
                       decoration: InputDecoration(
                           labelText: "Server URL",
                           hintText: "http://xxx:xxx/xxx")),
                   TextFormField(
                     obscureText: true,
+                    onSaved: (s) => _encode = s,
                     decoration: InputDecoration(
                         labelText: "Passphrase (Optional)",
                         helperText:
@@ -45,10 +57,11 @@ class SettingsState extends State<Settings> {
                   ),
                   Container(
                       child: new Switch(
-                          value: _value,
+                          value: _run,
                           onChanged: (newValue) {
                             setState(() {
-                              _value = newValue;
+                              _run = newValue;
+                              run();
                             });
                           },
                           inactiveThumbColor: Colors.grey,
@@ -58,4 +71,41 @@ class SettingsState extends State<Settings> {
               ),
             )));
   }
+
+  void run() async {
+    if (_run && _port == null) {
+      print('To run message fetch');
+      var receivePort = ReceivePort();
+      receivePort.listen((msg) {
+        _port = msg as SendPort;
+        if (_run == false) {
+          _port.send(true);
+        }
+      });
+//      AndroidAlarmManager.oneShot(Duration(milliseconds: 500), 99,
+//              () => runFetchMsg(_url, _encode, receivePort.sendPort))
+//          .catchError((e) => {print(e)})
+//          .then((bool) {
+//        print('run $bool');
+//      });
+    } else if (_port != null) {
+      print('To stop message fetch');
+      _port.send(true);
+    }
+  }
 }
+
+//void runFetchMsg(String url, String encode, SendPort sendPort) {
+//  print('Init sms receiver');
+//  SmsReceiver receiver = SmsReceiver();
+//  var listen = receiver.onSmsReceived.listen((SmsMessage msg) {
+//    print(msg.body);
+//  });
+//  var receivePort = ReceivePort();
+//  receivePort.listen((msg) {
+//    print('Cancel task');
+//    listen.cancel();
+//  });
+//  sendPort.send(receivePort.sendPort);
+//  print('Send port');
+//}
